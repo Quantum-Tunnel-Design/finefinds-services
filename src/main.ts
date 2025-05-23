e30325dbd45a7191420e55b58621cc495d845669
Amal Gamage <amal.gamage@mycomera.com>
2025-05-23 13:06:05 +0400

    fix: updated [skip deploy]

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,7 @@ async function bootstrap() {
   );
   app.use(compression());
   app.enableCors({
-    origin: configService.get('CORS_ORIGIN', '*'),
+    origin: configService.get('CORS_ORIGIN', 'http://localhost:3001'),
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
