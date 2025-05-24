3ad8f559eaee5ffc5d25ef980309c76ae4fd21c6
Amal Gamage <amalcg989@gmail.com>
2025-05-24 16:07:28 +0400

    fix: updated vendor profile and schema

--- src/vendor-profile/vendor-profile.module.ts
+++ src/vendor-profile/vendor-profile.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { VendorProfileService } from './vendor-profile.service';
 import { VendorProfileResolver } from './vendor-profile.resolver';
 import { PrismaModule } from '../prisma/prisma.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UsersModule],
   providers: [VendorProfileService, VendorProfileResolver],
   exports: [VendorProfileService],
 })
